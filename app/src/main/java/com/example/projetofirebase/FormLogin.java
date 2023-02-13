cb2d5429b22b34b8d0bd271ed7df004cee16da5b
sergio rodrigues <sergio.n4todev@gmail>
2023-02-13 01:21:29 -0300

    Subindo projeto fireBase tela login

---
+++ app/src/main/java/com/example/projetofirebase/FormLogin.java
@@ -0,0 +1,35 @@
+package com.example.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+        IniciarComponenetes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent (FormLogin.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void IniciarComponenetes(){
+
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+
+    }
+}
