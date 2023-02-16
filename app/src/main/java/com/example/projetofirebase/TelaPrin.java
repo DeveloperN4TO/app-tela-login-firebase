b84fe24cab45be42c1b949f2097418ae3c412cac
sergio rodrigues <sergio.n4todev@gmail>
2023-02-16 00:14:48 -0300

    Subindo finalização do layout
    Add nova tela principal: responsavel pelo perfil do usuario
    ja autenticado

--- app/src/main/java/com/example/projetofirebase/TelaPrin.java
+++ app/src/main/java/com/example/projetofirebase/TelaPrin.java
@@ -2,23 +2,13 @@ package com.example.projetofirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class FormCadastro extends AppCompatActivity {
-
+public class TelaPrin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_cadastro);
-
-
-
-            }
-        }
-
-
-
+        setContentView(R.layout.activity_tela_prin);
+    }
+}
