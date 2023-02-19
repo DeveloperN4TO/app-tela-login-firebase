458eb690bb718479571ec7f1674be69b6b2d5f5f
sergio rodrigues <sergio.n4todev@gmail>
2023-02-19 13:15:56 -0300

    Registramos todos ids dos nosso componentes

--- app/src/main/java/com/example/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/example/projetofirebase/FormCadastro.java
@@ -1,15 +1,18 @@
 package com.example.projetofirebase;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import java.util.Objects;
-
 public class FormCadastro extends AppCompatActivity {
+
+    private EditText edit_nome_cadastro;
+    private EditText edit_cadastro_email;
+    private EditText edit_cadastro_senha;
+
+    private Button button_cadastro;
 
 
     @Override
@@ -17,7 +20,17 @@ public class FormCadastro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
 
+        IniciarComponentes();
 
+
+            }
+
+            private void IniciarComponentes(){
+
+        edit_nome_cadastro = findViewById(R.id.edit_nome_cadastro);
+        edit_cadastro_email = findViewById(R.id.edit_cadastro_email);
+        edit_cadastro_senha = findViewById(R.id.edit_cadastro_senha);
+        button_cadastro = findViewById(R.id.button_cadastro);
 
 
 
