e1810719bee5730b704e0ab72851431dc0de547b
sergio rodrigues <sergio.n4todev@gmail>
2023-02-19 10:48:36 -0300

    Subindo integração do projeto com firebase

--- app/src/main/java/com/example/projetofirebase/MainActivity.java
+++ app/src/main/java/com/example/projetofirebase/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.projetofirebase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
