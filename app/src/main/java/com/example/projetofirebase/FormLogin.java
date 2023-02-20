c000731741b6235aa72d76a5dfac154527bfe348
sergio rodrigues <sergio.n4todev@gmail>
2023-02-19 23:14:51 -0300

    Novos componentes

--- app/src/main/java/com/example/projetofirebase/FormLogin.java
+++ app/src/main/java/com/example/projetofirebase/FormLogin.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class FormLogin extends AppCompatActivity {
 
@@ -61,7 +65,7 @@ public class FormLogin extends AppCompatActivity {
                     snackbar.show();
 
                 }else {
-                    AutenticarUsuario();
+                    AutenticarUsuario(view);
                 }
 
             }
@@ -69,7 +73,7 @@ public class FormLogin extends AppCompatActivity {
 
     }
 
-    private void AutenticarUsuario(){
+    private void AutenticarUsuario(View view){
 
         String email = edit_email.getText().toString();
         String senha = edit_senha.getText().toString();
@@ -89,7 +93,18 @@ public class FormLogin extends AppCompatActivity {
                         }
                     },3000);
 
+                }else  {
+                    String erro;
 
+                    try {
+                        throw Objects.requireNonNull(task.getException());
+                    }catch (Exception e){
+                        erro = "Erro ao logar usuário";
+                    }
+                    Snackbar snackbar = Snackbar.make(view, erro,Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.RED);
+                    snackbar.setTextColor(Color.WHITE);
+                    snackbar.show();
 
                 }
 
@@ -100,7 +115,19 @@ public class FormLogin extends AppCompatActivity {
 
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if( usuarioAtual != null){
+           TelaPrin telaPrin = new TelaPrin();
+        }
+    }
+
     private void TelaPrincipal(){
+        Log.d("LOGIN", "Abrindo tela principal");
         Intent intent = new Intent(FormLogin.this,TelaPrin.class);
         startActivity(intent);
         finish();
