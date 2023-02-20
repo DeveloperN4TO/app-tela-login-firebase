159ca6eab061014b2fb094fdbf02bf293ed9ac2a
sergio rodrigues <sergio.n4todev@gmail>
2023-02-19 21:04:03 -0300

    Novas atualizaçoes
    Foi implementado novos tratamentos de erro
    incluimos tambem uma cor ver para exibição
    caso o usuario foi cadastrado com sucesso

---
+++ app/src/main/java/com/example/projetofirebase/DocumentoReference.java
@@ -0,0 +1,4 @@
+package com.example.projetofirebase;
+
+public class DocumentoReference {
+}
--- app/src/main/java/com/example/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/example/projetofirebase/FormCadastro.java
@@ -2,15 +2,26 @@ package com.example.projetofirebase;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class FormCadastro extends AppCompatActivity {
@@ -22,6 +33,7 @@ public class FormCadastro extends AppCompatActivity {
     private Button button_cadastro;
 
     String[] mensagens = {"Preencha todos os campos", "Cadastro Realizado com sucesso!"};
+    String usuarioID;
 
 
     @Override
@@ -62,8 +74,10 @@ public class FormCadastro extends AppCompatActivity {
 
                     if (task.isSuccessful()){
 
+                        SalvarDadosUsuario();
+
                         Snackbar snackbar = Snackbar.make(view, mensagens [1],Snackbar.LENGTH_LONG);
-                        snackbar.setBackgroundTint(Color.RED);
+                        snackbar.setBackgroundTint(Color.GREEN);
                         snackbar.setTextColor(Color.WHITE);
                         snackbar.show();
                     }else{
@@ -98,6 +112,40 @@ public class FormCadastro extends AppCompatActivity {
                     }
 
                 });
+
+            }
+
+            private  void SalvarDadosUsuario(){
+
+            String nome = edit_nome_cadastro.getText().toString();
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                Map<String,Object> usuarios = new HashMap<>();
+                usuarios.put("nome",nome);
+
+                usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                DocumentReference documentoReference = db.collection("Usuarios").document(usuarioID);
+                documentoReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        Log.d("db", "Sucesso ao salvar os dados");
+
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("db_error", "Erro ao salvar os dados" + e.toString());
+
+                            }
+                        });
+
+
+
+
 
             }
 
