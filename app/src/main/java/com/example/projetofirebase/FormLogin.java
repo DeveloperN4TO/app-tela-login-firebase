f595bfaf1c0737b8bb5c83aa4314788ab676c167
sergio rodrigues <sergio.n4todev@gmail>
2023-02-19 22:16:49 -0300

    Nova atualização
    ProgressBar configurada para 3 segundos
    
    nosso app ja esta apto a receber usuarios cadastrados e autenticados.

--- app/src/main/java/com/example/projetofirebase/FormLogin.java
+++ app/src/main/java/com/example/projetofirebase/FormLogin.java
@@ -1,15 +1,34 @@
 package com.example.projetofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_tela_cadastro;
+
+    private EditText edit_email;
+    private EditText edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressbar;
+
+    String[] mensagens = {"Preencha todos os campos", "Login efetuado com sucesso!"};
     
 
     @Override
@@ -27,10 +46,74 @@ public class FormLogin extends AppCompatActivity {
             }
         });
 
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+
+                    Snackbar snackbar = Snackbar.make(view, mensagens [0],Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.RED);
+                    snackbar.setTextColor(Color.WHITE);
+                    snackbar.show();
+
+                }else {
+                    AutenticarUsuario();
+                }
+
+            }
+        });
+
+    }
+
+    private void AutenticarUsuario(){
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    progressbar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            TelaPrincipal();
+
+                        }
+                    },3000);
+
+
+
+                }
+
+            }
+        });
+
+
+
+    }
+
+    private void TelaPrincipal(){
+        Intent intent = new Intent(FormLogin.this,TelaPrin.class);
+        startActivity(intent);
+        finish();
+
+
     }
     private void IniciarComponenetes(){
 
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressbar = findViewById(R.id.progressbar);
 
     }
 }
